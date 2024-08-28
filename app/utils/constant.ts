efc4dde395eebc2a5bf5d70c1a7cac2c3ebdec59
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-08-28 08:17:20 +0000

    update meta description

--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -16,7 +16,7 @@ export const site: Site = {
   baseUrl: 'https://zira.my.id',
   author: 'Fauzira Alpiandi',
   title: 'Fauzira Alpiandi',
-  desc: 'It‘s a multifaceted creative with expertise in frontend development, artwork, and writing.',
+  desc: 'Do you really like bright objects at night? That\u2019s always the silliest question I\u2019ve ever heard. More annoying than those clouds that often block everything and force to hit me the hay early.',
   locale: 'en-US',
   timeZone: 'Asia/Jakarta',
   verify: 'xuMdCxKom7IZ2YwCTzVJli3Sp_bvt-nofj8Q1iBjPf0',

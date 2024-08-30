8ad4bf520c6b1cef9004d9e6c19329955aae29d8
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-08-30 19:27:37 +0000

    UI

--- app/utils/constant.ts
+++ app/utils/constant.ts
@@ -16,7 +16,7 @@ export const site: Site = {
   baseUrl: 'https://zira.my.id',
   author: 'Fauzira Alpiandi',
   title: 'Fauzira Alpiandi',
-  desc: 'Do you really like bright objects at night? That\u2019s always the silliest question I\u2019ve ever heard. More annoying than those clouds that often block everything and force to hit me the hay early.',
+  desc: '\u2014and it kinda feels like I\u2019m trying to save some paper with what you\u2019re seeing right now; nothing\u2019s colored here\u2014 keep it simple, stupid!',
   locale: 'en-US',
   timeZone: 'Asia/Jakarta',
   verify: 'xuMdCxKom7IZ2YwCTzVJli3Sp_bvt-nofj8Q1iBjPf0',

f138b65e7b43b06d38fcbecd4b641a2c2a7c4262
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-09-01 17:59:01 +0000

    adding RSS

---
+++ app/rss/route.ts
@@ -0,0 +1,46 @@
+import { site } from 'app/utils/constant'
+import { getPosts } from 'app/utils/provider'
+
+export async function GET() {
+  let allBlogs
+
+  try {
+    allBlogs = await getPosts()
+  } catch (error) {
+    console.error('Failed to fetch posts:', error)
+    return new Response('Failed to fetch posts', { status: 500 })
+  }
+
+  const itemsXml = allBlogs
+    .sort(
+      (a, b) =>
+        new Date(b.metadata.date).getTime() -
+        new Date(a.metadata.date).getTime(),
+    )
+    .map(
+      (post) => `
+        <item>
+          <title>${post.metadata.title}</title>
+          <link>${site.baseUrl}/posts/${post.slug}</link>
+          <description>${post.metadata.summary || ''}</description>
+          <pubDate>${new Date(post.metadata.date).toUTCString()}</pubDate>
+        </item>`,
+    )
+    .join('')
+
+  const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
+  <rss version="2.0">
+    <channel>
+        <title>${site.title}</title>
+        <link>${site.baseUrl}</link>
+        <description>RSS Feed</description>
+        ${itemsXml}
+    </channel>
+  </rss>`
+
+  return new Response(rssFeed, {
+    headers: {
+      'Content-Type': 'text/xml',
+    },
+  })
+}

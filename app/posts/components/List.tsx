f03192f29a2ca33f426164d0ee4d8d3b615a1809
fauziralpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-09-10 18:26:25 +0000

    default value

--- app/posts/components/List.tsx
+++ app/posts/components/List.tsx
@@ -35,14 +35,17 @@ export async function MyPosts({ featured, recent }: FilteredPostsProps) {
           key={post.slug}
           className="flex flex-col my-4 space-y-2"
           href={`/posts/${post.slug}`}
-          aria-label={`Read more about ${post.metadata.title}`}
+          aria-label={`Read more about ${post.metadata.title || 'Untitled'}`}
         >
           <div className="flex flex-col md:flex-row md:justify-between w-full">
             <p className="text-neutral-400 tabular-nums md:order-2">
-              {formatDate(post.metadata.date, 'absolute')}
+              {formatDate(
+                post.metadata.date || new Date().toISOString(),
+                'absolute',
+              )}
             </p>
             <p className="font-medium text-neutral-200 leading-snug md:order-1">
-              {post.metadata.title}
+              {post.metadata.title || 'Untitled'}
             </p>
           </div>
         </Link>

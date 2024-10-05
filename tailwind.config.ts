ea0f0d4a3a597a6dea569b093d6d002be771eaaf
Fauzira Alpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-10-05 19:32:06 +0000

    styles option

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 import typography from '@tailwindcss/typography'
 
 export default {
-  content: ['./app/**/*.{md,mdx,ts,tsx}', './content/**/*.{md,mdx,ts,tsx}'],
+  content: ['./app/**/*', './content/**/*'],
   theme: {
     extend: {
       typography: {

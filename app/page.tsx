78765d534c72dc8e3610f41ad3a2f8bf8a6c2cc4
Fauzira Alpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-10-08 21:28:56 +0000

    modular

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,6 @@
 import React from 'react'
 
-import { site } from 'app/site'
-
-function FlipName() {
-  return (
-    <h1 className="mb-8">
-      <span className="sr-only">{site.author}</span>
-      <span aria-hidden="true" className="block overflow-hidden group relative">
-        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full">
-          {site.author.split('').map((letter, index) => (
-            <span
-              key={index}
-              className="inline-block"
-              style={{ transitionDelay: `${index * 25}ms` }}
-            >
-              {letter === ' ' ? '\u00A0' : letter}
-            </span>
-          ))}
-        </span>
-        <span className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
-          {site.alias.split('').map((letter, index) => (
-            <span
-              key={index}
-              className="inline-block"
-              style={{ transitionDelay: `${index * 25}ms` }}
-            >
-              {letter}
-            </span>
-          ))}
-        </span>
-      </span>
-    </h1>
-  )
-}
+import { FlipName } from 'components/FlipName'
 
 export default function Home() {
   return (
---
+++ components/FlipName.tsx
@@ -0,0 +1,33 @@
+import { site } from 'app/site'
+
+export function FlipName() {
+  return (
+    <h1 className="mb-8">
+      <span className="sr-only">{site.author}</span>
+      <span aria-hidden="true" className="block overflow-hidden group relative">
+        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full">
+          {site.author.split('').map((letter, index) => (
+            <span
+              key={index}
+              className="inline-block"
+              style={{ transitionDelay: `${index * 25}ms` }}
+            >
+              {letter === ' ' ? '\u00A0' : letter}
+            </span>
+          ))}
+        </span>
+        <span className="inline-block absolute left-0 top-0 transition-all duration-300 ease-in-out translate-y-full group-hover:translate-y-0">
+          {site.alias.split('').map((letter, index) => (
+            <span
+              key={index}
+              className="inline-block"
+              style={{ transitionDelay: `${index * 25}ms` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </span>
+      </span>
+    </h1>
+  )
+}

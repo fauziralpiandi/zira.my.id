013e47aa6a07e81f11ece6b830874627bb486d19
Fauzira Alpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-10-14 18:42:32 +0000

    global styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,55 +3,22 @@ import typography from '@tailwindcss/typography'
 
 export default {
   darkMode: 'selector',
-  content: ['./app/**/*.{md,mdx,ts,tsx}', './components/**/*.{md,mdx,ts,tsx}'],
+  content: ['./app/**/*.{md,mdx,ts,tsx}'],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      mono: {
-        50: '#eeeeee',
-        100: '#e6e6e6',
-        200: '#cccccc',
-        300: '#b3b3b3',
-        400: '#999999',
-        500: '#808080',
-        600: '#666666',
-        700: '#4d4d4d',
-        800: '#333333',
-        900: '#1c1c1c',
-        950: '#111111',
-      },
-      hyper: {
-        50: '#ffffe0',
-        100: '#ffffb3',
-        200: '#ffff80',
-        300: '#ffff4d',
-        400: '#ffff1a',
-        500: '#ffd700',
-        600: '#ccac00',
-        700: '#b38f00',
-        800: '#996b00',
-        900: '#664400',
-        950: '#333300',
-      },
-    },
     extend: {
-      typography: {
-        DEFAULT: {
-          css: {
-            'code::before': {
-              display: 'none',
-            },
-            'code::after': {
-              display: 'none',
-            },
-            'p::before': {
-              display: 'none',
-            },
-            'p::after': {
-              display: 'none',
-            },
-          },
+      colors: {
+        mono: {
+          50: '#eeeeee',
+          100: '#e6e6e6',
+          200: '#cccccc',
+          300: '#b3b3b3',
+          400: '#999999',
+          500: '#808080',
+          600: '#666666',
+          700: '#4d4d4d',
+          800: '#333333',
+          900: '#1c1c1c',
+          950: '#111111',
         },
       },
     },

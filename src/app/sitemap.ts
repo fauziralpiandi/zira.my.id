7ffd097fd3959f5eb8c698062712041aa0f350e5
Fauzira Alpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2024-11-17 23:34:25 +0700

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 export default async function sitemap() {
   return [
     {
-      url: 'https://zira.my.id',
+      url: 'https://zira.my.id/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,

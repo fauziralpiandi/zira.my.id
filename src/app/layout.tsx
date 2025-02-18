7c629b62996d3a5035bf5b0e7788287e614a5944
Fauzira Alpiandi <165820519+fauziralpiandi@users.noreply.github.com>
2025-02-18 02:37:43 +0000

    (fix)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const viewport: Viewport = {
   colorScheme: 'dark',
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1.5,
+  // maximumScale: 1,
 };
 
 export const metadata: Metadata = {
--- src/components/StoryCard.tsx
+++ src/components/StoryCard.tsx
@@ -9,15 +9,15 @@ export const StoryCard = ({ post }: { post: Stories }) => {
 
   return (
     <div className="group h-full overflow-hidden">
-      <Link href={`stories/${slug}`} className="group flex h-full flex-col">
-        <figure className="relative aspect-3/2 overflow-hidden rounded-md border border-neutral-900 bg-neutral-900">
+      <Link href={`stories/${slug}`} className="flex h-full flex-col">
+        <figure className="relative aspect-3/2 overflow-hidden border border-neutral-900 bg-neutral-900">
           <Image
             src={image}
             alt={title}
             fill
             className="animate object-cover group-hover:scale-110"
           />
-          <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end bg-neutral-950/75 p-4 backdrop-blur-sm backdrop-grayscale">
+          <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end bg-neutral-950/75 p-4 backdrop-blur backdrop-grayscale">
             <div className="flex items-center space-x-1.5">
               <time
                 className="font-display text-accent text-xs font-medium"

6522eaa15259b1baeec4722751f4ded722c9adef
Fauzira Alpiandi <fwzyrln@gmail.com>
2025-06-04 16:16:40 +0000

    desc

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -15,8 +15,7 @@ type NavItem = {
 const constant: Constant = {
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL!,
   title: 'fwzyrln_',
-  description:
-    'A West Java-based Scribe Developer\u2014Naturally a Selenophile.',
+  description: 'An Engineer Defined by Intent \u2014 Lunar Enthusiast.',
   authorName: 'Fauzira Alpiandi',
   locale: 'en-US',
   timeZone: 'Asia/Jakarta',
